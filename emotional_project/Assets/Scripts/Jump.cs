fb93cf41d84444ababb3f7cf6217871c50f5d383
milkaraien <milkaraien@gmail.com>
2025-04-14 23:13:53 +0300

    jumping works, walking in process

--- emotional_project/Assets/Scripts/Jump.cs
+++ emotional_project/Assets/Scripts/Jump.cs
@@ -2,35 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : StateMachineBehaviour
+public class Jump : MonoBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    [SerializeField] private Animator _animator;
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    private void Start()
+    {
+        
+    }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("Jump");
+        }
+    }
 }
---
+++ emotional_project/Assets/Scripts/Walk.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float speed = 5f; // Скорость движения персонажа
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            float moveInput = Input.GetAxis("Horizontal"); // Получаем входные данные по оси X (A/D или стрелки)
+
+            // Устанавливаем скорость движения персонажа
+            _rb.velocity = new Vector2(moveInput * speed, _rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _animator.SetTrigger("Walk");
+        }
+    }
+}
